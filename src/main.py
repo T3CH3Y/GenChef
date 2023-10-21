d2a2c81d5c69998c99ee68f6c89f251c0346f572
Ben Zimmerman <business@benzimmmerman.org>
2023-10-21 16:26:10 -0400

    AHHH

---
+++ src/main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import openai
+from config import config
+import os
+import sys
+
+openai.api_key = config["OPENAI_KEY"]
+
+
+
+if __name__ == "__main__":
+    st.title("Let GenChef Help You Make The Most Of Your Ingredients")
+    st.image("graphix/genchef.png")
+    query = st.text_input(label = "Tell the chef what you're working with.")
+    dish_image = openai.Image.create(prompt = query, n = 1, size = "256x256")
+    dish_image_clean = dish_image['data'][0]['url']
+    st.image(dish_image_clean)
+
