2903543db55f80b7435a29104d167cafc8485dd7
Ben Zimmerman <business@benzimmerman.org>
2023-10-21 16:37:31 -0400

    FIX THE DAMN THING

--- src/main.py
+++ src/main.py
@@ -12,12 +12,7 @@ if __name__ == "__main__":
     st.title("Let GenChef Help You Make The Most Of Your Ingredients")
     st.image("graphix/genchef.png")
     query = st.text_input(label = "Tell the chef what you're working with.")
-<<<<<<< HEAD
     dish_image = openai.Image.create(prompt = query, n = 1, size = "256x256")
     dish_image_clean = dish_image['data'][0]['url']
     st.image(dish_image_clean)
-=======
-    dish_image = openai.Image.create
-    
->>>>>>> parent of 3a1b163 (query stuff)
 
