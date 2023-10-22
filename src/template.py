a207592699e08c097159e6b1e4c34534bddf95f9
al1y3vk <AliyevK2003@gmail.com>
2023-10-21 22:39:50 -0400

    gonna change

--- src/template.py
+++ src/template.py
@@ -7,15 +7,20 @@ from llama_index.node_parser import SimpleNodeParser
 from llama_index.vector_stores import WeaviateVectorStore
 from llama_index import VectorStoreIndex, StorageContext
 
-client = weaviate.Client(embedded_options=weaviate.embedded.EmbeddedOptions(), additional_headers={ 'X-OpenAI-Api-Key': config["OPENAI_KEY"]})
+client = weaviate.Client(
+    url = "https://my-sandbox-csjz1k6w.weaviate.network",  # Replace with your endpoint
+    auth_client_secret=weaviate.AuthApiKey(api_key=config["WEAV_KEY"]),  # Replace w/ your Weaviate instance API key
+    additional_headers = {
+        "X-OpenAI-Api-Key": config["OPENAI_KEY"]
+    }
+)
 openai.api_key = config["OPENAI_KEY"]
 
 # load the blogs in using the reader
-blogs = SimpleDirectoryReader('src/testdata').load_data()
 
 # chunk up the blog posts into nodes
 parser = SimpleNodeParser.from_defaults(chunk_size=1024, chunk_overlap=20)
-nodes = parser.get_nodes_from_documents(blogs)
+
 
 # construct vector store
 vector_store = WeaviateVectorStore(weaviate_client = client, index_name="BlogPost", text_key="content")
@@ -24,7 +29,7 @@ vector_store = WeaviateVectorStore(weaviate_client = client, index_name="BlogPos
 storage_context = StorageContext.from_defaults(vector_store = vector_store)
 
 # set up the index
-index = VectorStoreIndex(nodes, storage_context = storage_context)
+
 
 query_engine = index.as_query_engine()
 response = query_engine.query("What is the intersection between LLMs and search?")
