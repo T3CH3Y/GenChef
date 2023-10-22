4d1925ac8c4f8caefe29acca7da85375334c799a
al1y3vk <AliyevK2003@gmail.com>
2023-10-21 23:47:49 -0400

    progress

--- src/config.py
+++ src/config.py
@@ -6,5 +6,6 @@ load_dotenv()
 config = {
     "OPENAI_KEY" : os.environ["OPENAI_KEY"],
     "WEAVIATE_KEY" : os.environ["WEAVIATE_KEY"],
-    "OPENAI_MODEL" : "gpt-3.5-turbo-instruct"
+    "OPENAI_MODEL" : "gpt-3.5-turbo-instruct",
+    "WEAVIATE_URL" : "https://my-sandbox-csjz1k6w.weaviate.network"
 }
--- src/template.py
+++ src/template.py
@@ -1,36 +1,118 @@
-import weaviate
-import openai
 from config import config
 
-from llama_index import SimpleDirectoryReader
-from llama_index.node_parser import SimpleNodeParser
+import os
+import openai
+import weaviate
+
+from langchain import HuggingFaceTextGenInference
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+
+from llama_index import (
+    LangchainEmbedding,
+    PromptHelper,
+    QuestionAnswerPrompt,
+    RefinePrompt,
+    ServiceContext,
+    VectorStoreIndex,
+)
+
+from llama_index.llm_predictor import LLMPredictor
 from llama_index.vector_stores import WeaviateVectorStore
-from llama_index import VectorStoreIndex, StorageContext
+from llama_index.callbacks import CallbackManager, LlamaDebugHandler
 
 client = weaviate.Client(
-    url = "https://my-sandbox-csjz1k6w.weaviate.network",  # Replace with your endpoint
+    url = config["WEAVIATE_URL"],  # Replace with your endpoint
     auth_client_secret=weaviate.AuthApiKey(api_key=config["WEAVIATE_KEY"]),  # Replace w/ your Weaviate instance API key
     additional_headers = {
         "X-OpenAI-Api-Key": config["OPENAI_KEY"]
     }
 )
+
 openai.api_key = config["OPENAI_KEY"]
 
-# load the blogs in using the reader
+if __name__ == "__main__":
+    weaviate_api_key = os.getenv("WEAVIATE_KEY")
+    weaviate_url = os.getenv("WEAVIATE_URL")
+    tgi_endpoint_url = os.getenv("OPENAI_MODEL")
 
-# chunk up the blog posts into nodes
-parser = SimpleNodeParser.from_defaults(chunk_size=1024, chunk_overlap=20)
+    if not weaviate_api_key or not weaviate_url or not tgi_endpoint_url:
+        exit(
+            "Make sure you've set WEAVIATE_API_TOKEN, WEAVIATE_URL, and TGI_ENDPOINT_URL environment variables"
+        )
 
+    auth_config = weaviate.AuthApiKey(api_key=weaviate_api_key)
 
-# construct vector store
-vector_store = WeaviateVectorStore(weaviate_client = client, index_name="BlogPost", text_key="content")
+    client = weaviate.Client(
+        url=weaviate_url,
+        auth_client_secret=auth_config,
+    )
 
-# setting up the storage for the embeddings
-storage_context = StorageContext.from_defaults(vector_store = vector_store)
+    embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
 
-# set up the index
+    llm_predictor = LLMPredictor(
+        llm=HuggingFaceTextGenInference(
+            inference_server_url=tgi_endpoint_url,
+            max_new_tokens=512,
+            streaming=True,
+        ),
+    )
 
+    llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+    callback_manager = CallbackManager([llama_debug])
 
-query_engine = index.as_query_engine()
-response = query_engine.query("What is the intersection between LLMs and search?")
-print(response)
+    service_context = ServiceContext.from_defaults(
+        embed_model=embed_model,
+        llm_predictor=llm_predictor,
+        prompt_helper=PromptHelper(context_window=1024),
+        callback_manager=callback_manager,
+    )
+
+    vector_store = WeaviateVectorStore(
+        weaviate_client=client, index_name="GenChef"
+    )
+
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, service_context=service_context
+    )
+
+    text_qa_template = QuestionAnswerPrompt(
+        """<s>[INST] <<SYS>>
+We have provided context information below. 
+
+{context_str}
+
+Given this information, please answer the question.
+<</SYS>>
+
+{query_str} [/INST]"""
+    )
+
+    refine_template = RefinePrompt(
+        """<s>[INST] <<SYS>>
+The original query is as follows: 
+
+{query_str}
+
+We have provided an existing answer:
+
+{existing_answer}
+
+We have the opportunity to refine the existing answer (only if needed) with some more context below.
+
+{context_msg}
+<</SYS>>
+
+Given the new context, refine the original answer to better answer the query. If the context isn't useful, return the original answer. [/INST]"""
+    )
+
+    query_engine = index.as_query_engine(
+        text_qa_template=text_qa_template,
+        refine_template=refine_template,
+        streaming=True,
+    )
+
+    response = query_engine.query(
+        "Give me a recipe for a chicken sandwich."
+    )
+    response.print_response_stream()
+    print(f"\n\nSources:\n\n{response.get_formatted_sources()}")
